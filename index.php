6973e8f55159ed69d7f6075b8585820fd79e137e
Ali <aliabduh.2020@gmail.com>
2022-03-06 19:14:49 +0300

    filter array

--- index.php
+++ index.php
@@ -1,37 +1,36 @@
-
 <!DOCTYPE html>
-
-<html>
-	<body>
-		<!--name.php to be called on form submission-->
-		<form method = 'post'>
-			<h4>SELECT SUBJECTS</h4>
-			
-			<select name = 'subject[]' multiple size = 6>
-				<option value = 'english'>ENGLISH</option>
-				<option value = 'maths'>MATHS</option>
-				<option value = 'computer'>COMPUTER</option>
-				<option value = 'physics'>PHYSICS</option>
-				<option value = 'chemistry'>CHEMISTRY</option>
-				<option value = 'hindi'>HINDI</option>
-			</select>
-			<input type = 'submit' name = 'submit' value = Submit>
-		</form>
-	</body>
-</html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
 <?php
+	// define array
+	$a = array(1, 5, 2, 5, 1, 3, 2, 4, 5);
 	
-	// Check if form is submitted successfully
-	if(isset($_POST["submit"]))
-	{
-		// Check if any option is selected
-		if(isset($_POST["subject"]))
-		{
-			// Retrieving each selected option
-			foreach ($_POST['subject'] as $subject)
-				print "You selected $subject<br/>";
-		}
-	else
-		echo "Select an option first !!";
-	}
+	// print original array
+	echo "Original Array : \n";
+	print_r($a);
+	
+	// remove duplicate values by using
+	// flipping keys and values
+	$a = array_flip($a);
+
+	// restore the array elements by again
+	// flipping keys and values.
+	$a = array_flip($a);
+
+	// re-order the array keys
+	$a= array_values($a);
+
+	// print updated array
+	echo "\nUpdated Array : \n ";
+	print_r($a);
 ?>
+
+</body>
+</html>
